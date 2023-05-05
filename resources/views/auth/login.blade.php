2008a76481f27cb4633f2194915285005dbe3f6f
Cristian González <crisgon1065@gmail.com>
2023-05-04 18:24:53 -0600

    merge

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,59 +1,71 @@
 <!doctype html>
 <html lang="en">
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/fonts.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+    <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style.css">
+    <!-- Template -->
+    <link rel="stylesheet" href="assets/css/login.css">
+    <!-- Scripts -->
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <title>Login</title>
+    <!-- Title -->
+    <title>Inicar Sesión</title>
+</head>
 <body>
-<div class="content">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <img src="images-landing/undraw_remotely_2j6y.svg" alt="Image" class="img-fluid">
-            </div>
-            <div class="col-md-6 contents">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="mb-4">
-                            <h3>Inicio de Sesión</h3>
-                            <p class="mb-4">Bienvenido, ingresa tus credenciales.</p>
+<div class="main-container">
+    <section class="ftco-section bg-img bg-cover content" style="background-image: url('/assets/img/background/coffee-login.jpg'); ">
+        <div class="container">
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-6 col-lg-5">
+                    <div class="login-wrap p-4 p-md-5">
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <img src="assets/img/logo.svg" alt="Image" class="img-fluid">
+                            <span class="fa fa-user-o"></span>
                         </div>
-                        <form id="form-login" method="POST" action="{{ route('login') }}">
+                        <h1 class="heading-section text-center">Inicio de Sesión</h1>
+                        <h3 class="text-center mb-4">Bienvenido, por favor ingresa tus credenciales.</h3>
+                        <form class="login-form" id="form-login" method="POST" action="{{ route('login') }}">
                             @csrf
-                            <label>Correo Electrónico</label>
-                            <div class="form-group last mb-3">
-                                <input type="text" class="form-control"  id="email" name="email" value="{{ old('email') }}" required  autofocus>
+                            <div class="form-group">
+                                <input type="text" class="form-control rounded-left"  id="email" name="email" value="{{ old('email') }}" placeholder="Correo Electrónico" required autofocus>
                             </div>
-                            <label>Contraseña</label>
-                            <div class="form-group last mb-3">
-                                <input type="password" class="form-control" id="password" name="password" value="{{ old('password') }}" required>
+                            <div class="form-group d-flex">
+                                <input type="password" class="form-control rounded-left" id="password" name="password" value="{{ old('password') }}" placeholder="Contraseña" required>
                             </div>
-                            <button type="submit" id="btn-login" class="btn btn-block btn-primary">
-                                Iniciar Sesión
-                                <span class="spinner-border spinner-border-sm" id="loader" role="status" hidden="hidden"></span>
-                            </button>
+
                             @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    ¿Olvidaste tu contraseña?
-                                </a>
+                                <div class="form-group">
+                                    <div class="text-right">
+                                        <a href="{{ route('password.request') }}">¿Olvidaste tu contraseña?</a>
+                                    </div>
+                                </div>
                             @endif
+                            <div class="form-group text-center">
+                                <button type="submit" class="btn btn-primary rounded submit mt-3 p-3 px-5">Iniciar Sesión</button>
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+    <footer class="footer py-3 bg-primary text-white-50">
+        <div class="container text-center ">
+            <small>Copyright &copy; Grupo 6</small>
+        </div>
+    </footer>
 </div>
+
 <script>
     const Toast = Swal.mixin({
         toast: true,
@@ -90,10 +102,6 @@
         })
     </script>
 @endif
-<script src="https://preview.colorlib.com/theme/bootstrap/login-form-07/js/jquery-3.3.1.min.js"></script>
-<script src="https://preview.colorlib.com/theme/bootstrap/login-form-07/js/popper.min.js"></script>
-<script src="https://preview.colorlib.com/theme/bootstrap/login-form-07/js/bootstrap.min.js"></script>
-<script src="https://preview.colorlib.com/theme/bootstrap/login-form-07/js/main.js"></script>
-<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"793dc65ff8da31c8","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.11.3","si":100}' crossorigin="anonymous"></script>
+
 </body>
 </html>
